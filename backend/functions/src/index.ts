d27f9a836f7095bd1e60ffff336f61afcb89249c
ShakeebAftab <shakeebaftab@gmail.com>
2021-11-20 13:11:03 +0500

    Basic route setup

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,6 +1,19 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
+import * as cors from "cors";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+// Routes
+import { router as folderRoutes } from "./api/folders";
+import { router as userRoutes } from "./api/users";
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/folders", folderRoutes);
+app.use("/api/v1/users", userRoutes);
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+exports.app = functions.https.onRequest(app);
